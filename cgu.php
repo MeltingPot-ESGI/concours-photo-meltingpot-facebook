a0d93a9617ae3c45a12684e1ecc1d89b805e91fc
FlorianDeParis <florian.tournay@hotmail.fr>
2015-04-08 16:49:55 +0200

    Ajout du CGU

---
+++ cgu.php
@@ -0,0 +1,155 @@
+REGLEMENT DU JEU-CONCOURS
+« LES PHOTOS TATOUAGES »
+
+ARTICLE 1 : OBJET
+
+Tatoo nous par actions simplifiée au capital de 50 000 000 Euros – enregistrée au Registre du Commerce et des Sociétés de Nanterre sous le numéro RCS 331 089 474 , ayant son siège social situé au 32 rue Jacques Ibert, 92300 Levallois Perret (ci-après la «Société Organisatrice») organise un jeu intitulé « Les photos Tatoo nous » (ci-après le «Jeu »), du 16 mars 2015 à 9 heures au 6 avril 2015 à 17h inclus, les heures de la France métropolitaine faisant foi.
+
+
+ARTICLE 2 : CONDITIONS DE PARTICIPATION
+
+2.1 Participants
+
+Le Jeu est gratuit et sans obligation d’achat et ouvert à toute personne physique majeure et capable âgée d’au moins 18 ans (Etat civil faisant foi), résidant en France Métropolitaine (y compris la Corse) disposant d’un accès internet et d'une adresse électronique personnelle (email) à laquelle elle pourra, le cas échéant, être contactée pour les besoins de la gestion du Jeu (ci-après le « Participant »).
+
+2.2 Conditions de validité de la participation
+
+La participation au Jeu emporte l’acceptation pleine et entière, sans réserve, du présent règlement (ci-après le « Règlement ») dans son intégralité, des règles de déontologie en vigueur sur Internet, des conditions générales d’utilisation du site « Facebook », des conditions générales de vente des fournisseurs des lots, ainsi que de la réglementation applicable aux jeu-concours. 
+
+Toute participation au Jeu incomplète, non-conforme aux conditions exposées dans le Règlement, comportant des indications fausses, falsifiées, erronées ou enregistrées après la date limite de participation (le 6 avril à 17h, heure de la France métropolitaine faisant foi) ne sera pas prise en compte et sera considérée comme nulle.
+
+Chaque Participant ne pourra jouer qu’une seule fois pendant toute la durée du Jeu (même nom, même adresse postale ou email). 
+
+
+ARTICLE 3 : MÉCANIQUE DU JEU
+
+3.1 Modalités de participation
+
+La participation au Jeu s’effectue exclusivement par le biais de la page Facebook de “Tatoo nous”  accessible à l’adresse : https://www.facebook.com/pages/Tatoo-nous/1404451319873347
+
+Pour participer au Jeu, le Participant devra cumulativement disposer d'un compte "Facebook" et être fan de la page Facebook “Tatoo nous”. Il est précisé que le compte Facebook est soumis aux conditions générales d’utilisation du site Facebook, et qu’il est administré sous la seule responsabilité de la société Facebook. 
+
+Pour participer au Jeu, le Participant doit :
+
+•    Se connecter sur la page Facebook de “Tatoo nous” à l’adresse susvisée, dans la rubrique « Tatoo nous » ; 
+
+•    Remplir les champs « nom, email, ville » du formulaire proposé, et cocher la case « J’accepte le règlement » après l’avoir lu et approuvé, et joindre une image respectant les conditions du Jeu et notamment les caractéristiques suivantes (« l’Image ») : 
+
+o    L’Image peut être de quelque nature que ce soit (abstrait, réel, dessin, photo….) ;
+
+o    L’Image doit représenter les vacances ;
+
+o    Un format .Jpg est obligatoire ;
+
+o    L’Image ne doit pas avoir été rendue publique précédemment ou publiée à des fins commerciales, à titre gracieux ou onéreux ; 
+
+o    L’Image doit être conforme à l'ordre public et aux bonnes mœurs (absence de références à caractère raciste, ethnique, religieux, idéologique, politique, obscène, pornographique, injurieux, dénigrant, diffamatoire ou portant atteinte à l'intégrité d'autrui….) ;
+
+o    Le Participant doit être l’auteur de l’Image, ce qu’il garantit ;
+
+o    L’Image doit constituer une œuvre originale et ne contenir aucun emprunt à une autre œuvre, ce que le Participant garantit ;
+
+o    L’Image doit être libre de droit et pouvoir être utilisée dans les conditions décrites ci-après, ce que le Participant garantit ;
+
+o    L’Image ne doit pas porter atteinte aux droits détenus par des tiers de quelque nature que ce soit et notamment les droits de propriété intellectuelle et le droit à l’image des personnes pouvant figurer sur l’Image. Il s’engage et garantit en effet à avoir obtenu, le cas échéant, l’autorisation des personnes figurant sur l’Image ou des propriétaires des biens photographiés (le cas échéant), compte tenu de l’usage envisagé ci-après, ce que le Participant garantit.
+
+En cas de réclamation d'un tiers, quel qu'il soit, concernant l’Image, la Société Organisatrice se réserve le droit à tout moment sans accord ou information préalable du Participant, de ne pas prendre en compte la participation.
+
+3.2. Utilisation de l’Image
+
+Le Participant est informé et accepte que l’Image puisse être utilisée, reproduite et diffusée par la Société Organisatrice ou les sociétés du groupe Tatoo nous, notamment dans le cadre du Jeu (i) sur la page Facebook “Tatoo nous” (ii) dans le cadre des communications autour du Jeu, et ultérieurement sur différents supports de communication (Brochure, affiches…) notamment dans un but de promotion des produits de la Société Organisatrice.
+
+Par conséquent, le Participant cède à la Société Organisatrice, à titre exclusif et gratuit, tous les droits de propriété intellectuelle, artistique ou industrielle attachés à l’Image. Cette cession est consentie pour le monde entier et pour une durée de 12 mois à compter de leur première utilisation par la Société Organisatrice à compter de leur mise en ligne dans le cadre du Jeu. 
+En conséquence, sans que ces indications soient limitatives et sans limitation de quantité, le Participant cède à la Société organisatrice, les droits de reproduction, de représentation et d’adaptation de l’Image, étant précisé que :
+- Le droit de reproduction concerne tous les procédés de fixation matérielle de l’Image connus ou non encore connus à la date des présentes qui permettent et permettront de communiquer l’Image au public d’une manière directe indirecte, et notamment, sans que ceci ne soit limitatif, tout moyen de reproduction, d’impression ou d’enregistrement sur tout support connu ou inconnu à ce jour (notamment internet, support multimédia, flyer, brochure, affiches, publicités sur lieux de vente….).
+-    Le droit de représentation s’entend notamment du droit de communiquer l’Image au public par tous les procédés et moyens actuellement connus ainsi que par ceux qui pourraient être découverts dans l’avenir, au moyen de tout support sur lequel elles sont reproduites (y compris dans les agences de voyages, virtuelles ou non), connu ou inconnu à ce jour (internet, support mobiles ou tablette numérique, stand salon, exposition…), et par tout moyen audiovisuel, tel que, sans que cela ne soit exhaustif, tout moyen de projection dans des lieux réunissant du public (notamment agences de voyages, stand de vente…), Internet, réseaux sociaux, y compris électronique ou informatique, tout moyen de communication tel qu’Internet ;
+-    Le droit d’adaptation de l’Image s’entend, notamment, du droit de dupliquer, détourner, isoler tels fragments de l’Image, de lui substituer d’autres fonds, de modifier le cadrage, les couleurs, le contraste, de numériser l’Image, de modifier son support, d’y ajouter tout commentaire, titre ou sous-titre, marque, élément graphique, de la sonoriser (musique et/ou texte) aux fins de reproduction et de représentation
+La cession des droits est notamment accordée pour toute destination relative aux activités de la Société Organisatrice et des sociétés, et particulièrement à des fins de communications interne, d’information du public, de promotion, de publicité et de commercialisation des produits qu’elle fabrique, conçoit et/ou commercialise.
+
+-    Le Participant autorise également la Société Organisatrice à utiliser son nom le cas échéant pour les besoins de crédit.
+
+3.3. Fonctionnement du Jeu
+
+-    Les Images conformes aux caractéristiques visées au 3.1 seront publiées par la Société Organisatrice pendant toute la durée du Jeu, dans l’application du jeu “Tatoo nous” sur la page Facebook de “Tatoo nous” à l’adresse https://www.facebook.com/pages/Tatoo-nous/1404451319873347 (ci-après « l’Application »).
+
+-    Les internautes pourront voter dans l’Application pour la ou les Images qu’ils aiment, en cliquant sur le bouton « j’aime » proposé sur Facebook. Les internautes pourront voter autant de fois que Facebook le leur permet.
+
+-    Les votes obtenus le cas échéant par le biais d’un « partage » de l’Image sur Facebook ne seront pas comptabilisés.
+
+-    A la fin du Jeu et selon les modalités précisées à l’article 5 :
+
+o    l’Image ayant obtenu le plus de votes pendant toute la durée du Jeu remportera le 1er lot. Dans ce cadre et dans l’hypothèse où plusieurs Participants obtiendraient le même nombre de votes, le gagnant du 1er lot sera désigné par tirage au sort parmi ces Participants ;
+o    l’Image ayant obtenu le moins de votes pendant toute la durée du Jeu remportera le 2ème lot. Dans ce cadre et dans l’hypothèse où plusieurs Participants obtiendraient le même nombre de votes, le gagnant du 2ème lot sera désigné par tirage au sort parmi ces Participants ;
+o    un tirage au sort parmi tous les Participants désignera le gagnant du 3ème lot.
+
+
+ARTICLE 4 : DONNES PERSONNELLES
+
+Les données à caractère personnel recueillies sur chaque Participant tant lors de la participation au Jeu que, le cas échéant, en vue de permettre la remise d’un lot, sont collectées et traitées conformément aux dispositions de la loi n° 78-17 modifiée du 6 janvier 1978 relative à l’informatique, aux fichiers et aux libertés (dite « Loi Informatique et Libertés »). 
+
+Les données collectées pourront être communiquées aux prestataires de service et aux sous-traitants auxquels la Société Organisatrice pourrait éventuellement faire appel pour les besoins de l’organisation et/ou de la gestion du Jeu, ainsi qu’à des partenaires commerciaux de la Société Organisatrice dans l’hypothèse où le Participant a coché la case « Partenaires » sur le formulaire de participation.
+
+La Société Organisatrice pourra, sous réserve de l’acceptation du Participant, utiliser les données collectées dans un but commercial, promotionnel et d’amélioration de la fourniture de ses services par le biais de différents supports (par exemple : envoi de courriels). 
+
+Ces données pourront également être utilisées dans le cadre d’études internes au Groupe TUI, auquel appartient la Société Organisatrice, destinés à améliorer les services proposés.
+
+En application de la Loi Informatique et Libertés, chaque Participant au Jeu dispose d'un droit d'accès, de rectification, de suppression et d’opposition portant sur les données personnelles les concernant. Tout Participant peut exercer ce droit sur simple demande écrite envoyée à l’adresse ci-dessous, en précisant ses nom, prénom, adresse postale et en joignant copie d’un justificatif d’identité.
+
+Dans la mesure où les données collectées sur chaque Participant dans le cadre du Jeu sont indispensables pour la prise en compte de sa participation et l’attribution du lot qu’il aurait éventuellement gagné, l’exercice par un Participant de son droit de retrait avant la fin du Jeu entraînera l’annulation automatique de sa participation au Jeu.
+
+ARTICLE 5 : RESPONSABILITÉS
+
+8.1    La responsabilité de la Société Organisatrice ne saurait être encourue : 
+    si un Participant était déconnecté accidentellement par l’opérateur téléphonique ou son fournisseur d’accès Internet,
+    si un Participant oubliait de saisir ses coordonnées ou communiquait des données non valides,
+    si un Participant subissait une panne technique quelconque (mauvais état de la ligne, du combiné).
+
+La participation au Jeu implique la connaissance et l'acceptation des caractéristiques et des limites des technologies utilisées par l'Internet et les technologies qui y sont liées, notamment en ce qui concerne les performances techniques, les temps de réponse pour consulter, interroger ou transférer des informations, les risques d'interruption, et plus généralement, les risques inhérents à toute connexion et transmission.
+
+En conséquence, la société Organisatrice ne saurait en aucune circonstance être tenue responsable, sans que cette liste soit limitative : 
+    de la transmission et/ou de la réception de toute donnée et/ou information sur Internet,
+    de tout dysfonctionnement du réseau Internet 
+    de défaillance de tout matériel de réception ou des lignes de communication,
+    de perte de tout courrier papier ou électronique 
+    des problèmes d'acheminement, 
+    des conséquences de tout virus, bogue informatique, anomalie, défaillance technique, 
+    d'intervention malveillante,
+    de toute défaillance technique, matérielle et logicielle de quelque nature, ayant empêché ou limité la possibilité de participer au Jeu ou ayant endommagé le système d'un joueur.
+
+La connexion de toute personne aux sites internet à partir desquels s’effectue la participation et la participation des Participants au Jeu se fait sous leur entière responsabilité
+
+Il appartient à tout Participant de prendre toutes les mesures appropriées de façon à protéger contre toute atteinte ses propres données et/ou logiciels stockés sur l’ordinateur utilisé pour participer au Jeu. La connexion de toute personne aux sites internet faisant l’objet du Jeu et la participation des Participants au Jeu se fait sous leur entière responsabilité.
+
+8.2    La Société Organisatrice et ses sous-traitants déclinent toute responsabilité pour tous les incidents qui pourraient survenir lors de la jouissance du lot et/ou fait de son utilisation et/ou de ses conséquences. 
+
+8.3    La Société Organisatrice pourra annuler ou suspendre tout ou partie du Jeu s'il apparaît que des fraudes ou des tentatives de fraude sont intervenues sous quelque forme que ce soit dans le cadre de la participation au Jeu. Elle se réserve, dans cette hypothèse, le droit de ne pas attribuer le lot aux fraudeurs ou personnes ayant tenté de frauder et/ou de poursuivre devant les juridictions compétentes les auteurs de ces fraudes ou tentatives de fraudes.
+
+La Société Organisatrice ne saurait toutefois encourir aucune responsabilité d'aucune sorte vis-à-vis des Participants du fait des fraudes éventuellement commises. Sera notamment considérée comme fraude le fait pour un Participant de s’inscrire puis de participer au Jeu sous un ou des prêtes noms fictifs ou empruntés à une ou plusieurs tierces personnes, chaque Participant devant s’inscrire et participer au Jeu sous son propre et unique nom. En cas de manquement ou de fraude de la part d’un Participant, la Société Organisatrice se réserve la faculté d’écarter de plein droit toute participation émanant de ce dernier, sans que celui-ci ne puisse revendiquer quoi que ce soit.
+
+
+ARTICLE 6 : MODIFICATION DU REGLEMENT
+
+La Société Organisatrice prendra toutes les mesures nécessaires pour le respect du Règlement et ne saurait en aucun cas encourir une quelconque responsabilité si, en cas de force majeure ou d’événements indépendants de sa volonté ou de nécessité justifiée, elle était amenée pour quelque raison que ce soit, de modifier les conditions, prolonger, écourter, suspendre, reporter ou annuler à tout moment le Jeu ou de modifier tout ou partie du Règlement.
+
+
+ARTICLE 7 : PROPRIETE INTELLECTUELLE 
+
+La reproduction, la représentation ou l'exploitation de tout ou partie des éléments composant le Jeu sont strictement interdites. Toutes les marques ou noms de produits cités sont des marques ou noms de produits déposés par leurs propriétaires respectifs.
+
+
+ARTICLE 8 : RÈGLEMENT
+
+La participation au Jeu implique l'acceptation pure et simple, sans réserve, du Règlement dans son intégralité.
+
+Le Règlement est consultable en ligne directement sur l’Application (page Facebook “Tatoo nous”).
+
+ARTICLE 9 : RECLAMATIONS
+
+Toute contestation ou réclamation relative au Jeu devra être formulée par écrit et adressée à l’adresse stipulée à l’article 1 du Règlement et ne pourra être prise en considération au-delà d’un délai d’un mois à compter de la clôture du Jeu.
+
+
+ARTICLE 10 : LOI APPLICABLE ET ATTRIBUTION DE JURIDICTION
+
+Le Règlement est soumis à la loi française. Toute difficulté relative à l’application, l’interprétation ou les suites du Règlement sera tranchée par la juridiction compétente dans le ressort de la Cour d’Appel de Paris.
+
+Si une ou plusieurs stipulations du Règlement étaient déclarées nulles et/ou non applicables, les autres clauses garderaient toute leur force et leur portée.
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <html>
 	<body>
 		HELLO WORLD !
+		<a class="GCU" href="./cgu.php">voir les cgu</a>
 	</body>
 </html>
