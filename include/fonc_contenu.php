c87662168bd029661c71aef55dae717938e9c014
darkterreur <darkterreur@yahoo.fr>
2015-04-20 12:40:02 +0200

    creation structure

---
+++ include/fonc_contenu.php
@@ -0,0 +1,37 @@
+<?php
+
+function get_login(){
+		$html = '';
+		
+		$html .=	'<div class="bloc_login">
+		   				<div style="height: 15%;margin-right: 2%;margin-top: 2%;">
+							<i style="font-size: 30px;float: right;" class="fa fa-cogs"></i>
+						</div>
+		   				<form method="post" name="form_login_admin" id="form_login_admin">
+		   					<div style="">
+								<div class="label">
+									<label>Login : </label>
+								</div>
+								<div class="input">
+									<input style="width: 75%;" type="text" name="login_admin" id="login_admin">
+								</div>
+							</div>
+		   					<div style="">
+								<div class="label">
+									<label>Password : </label>
+								</div>
+								<div class="input">
+									<input style="width: 75%;" type="password" name="pwd_admin" id="pwd_admin">
+								</div>
+							</div>
+		   					<div style="float: left;margin-left: 38%;margin-top: 1%;">
+								<div class="button" id="annuler" onclick="return false;">Reset</div>
+								<div class="button" style="margin-left: 6px;" id="valider" onclick="login();return false;">Send</div>				 
+							</div>
+		   				</form>						
+		   			</div>';
+				
+		return $html;
+	}
+	
+?>
--- index.php
+++ index.php
@@ -1,6 +1,32 @@
-<html>
-	<body>
-		HELLO WORLD !
-		<a class="GCU" href="./cgu.php">voir les cgu</a>
-	</body>
+<?php 
+	@session_start();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
+   <?php 
+   		include_once("./include/fonction.php");
+   		
+   		echo get_head();	
+   	?>   
+   <body>
+   		<div id="wrapper">
+   			<div class="under_wrapper">
+   				<div id="wrapper_admin">
+	   				
+   				</div>
+   			</div>
+   		</div>
+   		
+   </body>
+   <?php echo include_js(); ?>
 </html>
+
+
+<script>
+	$(document).ready(function() {
+		get_data_admin('tout');
+	});
+
+
+	
+</script>
