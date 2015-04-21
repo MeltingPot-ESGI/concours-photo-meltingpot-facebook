c214bed42dc815de22ed614546a4e902c78043b6
FlorianDeParis <florian.tournay@hotmail.fr>
2015-04-21 11:55:36 +0200

    ..

--- src/views/FormParticipateContestView.php
+++ src/views/FormParticipateContestView.php
@@ -60,7 +60,6 @@ class FormParticipateContestView extends View
                         <input type="submit" class="button" name="form_validate" value="Participer">
                       </form>
                     </div>
-
                     <div class="fb-like" data-share="true" data-width="450" data-show-faces="true"></div>
                     ';
 
