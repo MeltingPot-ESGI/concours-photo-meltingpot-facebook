4c0a74a7277623f929d212e9f2179bb15fed1394
FlorianDeParis <florian.tournay@hotmail.fr>
2015-04-21 11:03:38 +0200

    ..

--- src/views/FormParticipateContestView.php
+++ src/views/FormParticipateContestView.php
@@ -30,7 +30,7 @@ class FormParticipateContestView extends View
             $html .= "Vous êtes connecté en tant que ".$graphObject->getName();
             $html .= ' <img src="http://graph.facebook.com/'.$graphObject->getId().'/picture" alt="Facebook profile picture" height="42" width="42">';
         } else {
-            $html .= '<a class="fb-button" href="'.$loginUrl.'">S\'authentifier avec Facebook</a>';
+            $html .= '<a class="fb-button button" href="'.$loginUrl.'">S\'authentifier avec Facebook</a>';
         }
 
         $html .= '
