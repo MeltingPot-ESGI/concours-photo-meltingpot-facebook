bd62c8361d56e765da4a610423568d04476383df
adriensy <adrien.sy@gmail.com>
2015-06-03 19:42:39 +0200

    debug

--- photo_list.php
+++ photo_list.php
@@ -74,10 +74,10 @@ if (isset($_SESSION) && isset($_SESSION[FB_TOKEN]) && !empty($_SESSION[FB_TOKEN]
                                         $images = $graphObject->getProperty('images')->asArray();
 
                                         $image = $images[0];
-
+                                        
                                         $source = $image->source;
                                         
-                                        $title .= "<div class='fb-like' data-href='".URL_FOR_LIKE_BUTTON.$graphObject->getProperty('id')."' data-layout='standard' data-action='like' data-show-faces='true' data-share='true' style='height:24px;'></div>".$photo['name'];
+                                        $title = "<div class='fb-like' data-href='".URL_FOR_LIKE_BUTTON.$graphObject->getProperty('id')."' data-layout='standard' data-action='like' data-show-faces='true' data-share='true' style='height:24px;'></div>".$photo['name'];
                         ?>
                                         <a href="<?php echo $source; ?>" data-mfp-src="<?php echo $source; ?>" title="<?php echo $title; ?>" ><img src="<?php echo $source; ?>" title="plume sur tete" border="0" height="50" width="50" ></a>
                         <?php
