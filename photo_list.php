f0e45e939baa2108791e3d8aede784eab916b986
adriensy <adrien.sy@gmail.com>
2015-06-03 19:37:28 +0200

    debug

--- photo_list.php
+++ photo_list.php
@@ -81,7 +81,7 @@ if (isset($_SESSION) && isset($_SESSION[FB_TOKEN]) && !empty($_SESSION[FB_TOKEN]
                                 <script>
                                     window.fbAsyncInit = function() {
                                       FB.init({
-                                        appId      : '".APP_ID."',
+                                        appId      : '342576715932172',
                                         cookie: true,
                                         xfbml      : true,
                                         oauth: true,
