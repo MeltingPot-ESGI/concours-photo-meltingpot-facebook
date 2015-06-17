b771e1a72c531b058c05df86cb74e9b269a13d50
adriensy <adrien.sy@gmail.com>
2015-06-17 12:38:03 +0200

    facebook

--- participate.php
+++ participate.php
@@ -3,6 +3,7 @@
     include_once("ressource/include/fonction.php");
     
     error_reporting(E_ALL);
+    ini_set('display_errors', '1');
     session_start();
     
     use Facebook\FacebookSession;
