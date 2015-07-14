bd8c23ecfadc544d018139b303066a744f2ed2e2
adriensy <adrien.sy@gmail.com>
2015-07-14 18:23:10 +0200

    Bakc office

--- index.php
+++ index.php
@@ -41,6 +41,16 @@ if (isset($_SESSION) && isset($_SESSION[FB_TOKEN]) && !empty($_SESSION[FB_TOKEN]
     }
 }
 
+$stmtConcours = $pdo->prepare("SELECT * FROM \"Concours\" WHERE id = :id;");
+
+$stmtConcours->execute(
+    array(':id' => 1)
+);
+
+// Utilisateur existe dans la BDD
+$concours = $stmtConcours->fetch(PDO::FETCH_ASSOC);
+
+
 ?>
 
 <html>
@@ -62,121 +72,128 @@ if (isset($_SESSION) && isset($_SESSION[FB_TOKEN]) && !empty($_SESSION[FB_TOKEN]
                         <h1>CONCOURS PHOTO TATOUAGE</h1>
                         
                         <?php
-                        
-                        // Session
-                        $helperParticipate = new FacebookRedirectLoginHelper(REDIRECT_URL_PARTICIPATE);
-                        $loginUrlParticipate = $helperParticipate->getLoginUrl(array('scope' => 'publish_actions, user_photos'));
-                        
+                        if ((strtotime($concours['date_end']) < time()) || ($concours['is_stopped'])) {
                         ?>
-                        
-                        <a href="<?php echo $loginUrlParticipate; ?>" class="fb-participate-link fb-form-participate-infos">Participer au concours -></a>
-                        
                         <div class="parent-container">
+                            Le concours est terminé.
+                        </div>
                         <?php
-                            if ($session) {
-                                $stmtCount = $pdo->query("SELECT COUNT(*) as total_photos FROM \"Photos\";");
-                                $result = $stmtCount->fetch(PDO::FETCH_ASSOC);
-                                $total = $result['total_photos'];
-                                $photosParPage = 6;
-                                $nombreDePages = ceil($total / $photosParPage);
-                                $pageCourante = isset($_GET['currentPage']) ? (int) $_GET['currentPage'] : 1;
-                                
-                                $premierePhoto = ($pageCourante-1) * $photosParPage;
-                                
-                                $stmt = $pdo->query("SELECT * FROM \"Photos\" ORDER BY date_add DESC LIMIT ".$photosParPage." OFFSET ".$premierePhoto.";");
-                                
-                                while ($photo = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                                    try {
-                                        $request = new FacebookRequest(
-                                            $session,
-                                            'GET',
-                                            '/'.trim($photo['id_facebook'])
-                                        );
-
-                                        $response = $request->execute();
-
-                                        $graphObject = $response->getGraphObject();
-                                        $images = $graphObject->getProperty('images')->asArray();
-
-                                        $image = $images[0];
-
-                                        $source = $image->source;
-                                        
-                                        $dataFbHref = URL_FOR_LIKE_BUTTON.$graphObject->getProperty('id');
-                                        $dataPhotoName = $photo['name'];
+                        } else {
+                            // Session
+                            $helperParticipate = new FacebookRedirectLoginHelper(REDIRECT_URL_PARTICIPATE);
+                            $loginUrlParticipate = $helperParticipate->getLoginUrl(array('scope' => 'publish_actions, user_photos'));
+                            
                         ?>
-                                    <div class="photo-facebook">
-                                        <a class="photo-facebook-js" href="<?php echo $source; ?>" data-mfp-src="<?php echo $source; ?>"><img src="<?php echo $source; ?>" title="plume sur tete" border="0" height="100" width="100" ></a>
-                                        <div id='fb-root'></div>
-                                        <p><?php echo $dataPhotoName; ?></p>
-                                        <div class='fb-like' data-href='<?php echo $dataFbHref; ?>' data-layout='button_count' data-action='like' data-show-faces='false' data-share='false'></div>
-                                    </div>
-                        <?php
-                                    } catch (Exception $e) {
-                                        continue;
+                        
+                            <a href="<?php echo $loginUrlParticipate; ?>" class="fb-participate-link fb-form-participate-infos">Participer au concours -></a>
+
+                            <div class="parent-container">
+                            <?php
+                                if ($session) {
+                                    $stmtCount = $pdo->query("SELECT COUNT(*) as total_photos FROM \"Photos\";");
+                                    $result = $stmtCount->fetch(PDO::FETCH_ASSOC);
+                                    $total = $result['total_photos'];
+                                    $photosParPage = 6;
+                                    $nombreDePages = ceil($total / $photosParPage);
+                                    $pageCourante = isset($_GET['currentPage']) ? (int) $_GET['currentPage'] : 1;
+
+                                    $premierePhoto = ($pageCourante-1) * $photosParPage;
+
+                                    $stmt = $pdo->query("SELECT * FROM \"Photos\" ORDER BY date_add DESC LIMIT ".$photosParPage." OFFSET ".$premierePhoto.";");
+
+                                    while ($photo = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                                        try {
+                                            $request = new FacebookRequest(
+                                                $session,
+                                                'GET',
+                                                '/'.trim($photo['id_facebook'])
+                                            );
+
+                                            $response = $request->execute();
+
+                                            $graphObject = $response->getGraphObject();
+                                            $images = $graphObject->getProperty('images')->asArray();
+
+                                            $image = $images[0];
+
+                                            $source = $image->source;
+
+                                            $dataFbHref = URL_FOR_LIKE_BUTTON.$graphObject->getProperty('id');
+                                            $dataPhotoName = $photo['name'];
+                            ?>
+                                        <div class="photo-facebook">
+                                            <a class="photo-facebook-js" href="<?php echo $source; ?>" data-mfp-src="<?php echo $source; ?>"><img src="<?php echo $source; ?>" title="plume sur tete" border="0" height="100" width="100" ></a>
+                                            <div id='fb-root'></div>
+                                            <p><?php echo $dataPhotoName; ?></p>
+                                            <div class='fb-like' data-href='<?php echo $dataFbHref; ?>' data-layout='button_count' data-action='like' data-show-faces='false' data-share='false'></div>
+                                        </div>
+                            <?php
+                                        } catch (Exception $e) {
+                                            continue;
+                                        }
                                     }
-                                }
-                                
-                                $sHtml = "<div class='facebook-pagination'>";
-                                
-                                if ($nombreDePages > 6) {
-                                    $sHtml .= getLinkPage(1);
 
-                                    if ($pageCourante > 1) {
-                                        $sHtml .= getLinkPage(($pageCourante-1), '<');
-                                    }
+                                    $sHtml = "<div class='facebook-pagination'>";
 
-                                    $sHtml .= getLinkPage(1);
+                                    if ($nombreDePages > 6) {
+                                        $sHtml .= getLinkPage(1);
 
-                                    if ($pageCourante >= ($nombreDePages-2)) {
-                                        $sHtml .= "...";
-                                        $sHtml .= getLinkPage(($nombreDePages-3));
-                                        $sHtml .= getLinkPage(($nombreDePages-2));
-                                        $sHtml .= getLinkPage(($nombreDePages-1));
-                                    } elseif ($pageCourante > 3 && $pageCourante < ($nombreDePages-2)) {
-                                        $pagePrecedente = ($pageCourante-1);
-                                        $pageSuivante = ($pageCourante+1);
+                                        if ($pageCourante > 1) {
+                                            $sHtml .= getLinkPage(($pageCourante-1), '<');
+                                        }
 
-                                        $sHtml .= "...";
-                                        $sHtml .= getLinkPage($pagePrecedente);
-                                        $sHtml .= getLinkPage($pageCourante);
-                                        $sHtml .= getLinkPage($pageSuivante);
-                                        $sHtml .= "...";
+                                        $sHtml .= getLinkPage(1);
+
+                                        if ($pageCourante >= ($nombreDePages-2)) {
+                                            $sHtml .= "...";
+                                            $sHtml .= getLinkPage(($nombreDePages-3));
+                                            $sHtml .= getLinkPage(($nombreDePages-2));
+                                            $sHtml .= getLinkPage(($nombreDePages-1));
+                                        } elseif ($pageCourante > 3 && $pageCourante < ($nombreDePages-2)) {
+                                            $pagePrecedente = ($pageCourante-1);
+                                            $pageSuivante = ($pageCourante+1);
+
+                                            $sHtml .= "...";
+                                            $sHtml .= getLinkPage($pagePrecedente);
+                                            $sHtml .= getLinkPage($pageCourante);
+                                            $sHtml .= getLinkPage($pageSuivante);
+                                            $sHtml .= "...";
+                                        } else {
+                                            $sHtml .= getLinkPage(2);
+                                            $sHtml .= getLinkPage(3);
+                                            $sHtml .= getLinkPage(4);
+                                            $sHtml .= "...";
+                                        } 
+
+                                        $sHtml .= getLinkPage($nombreDePages);
+
+                                        if ($pageCourante < $nombreDePages) {
+                                            $sHtml .= getLinkPage(($pageCourante+1), ">");
+                                        }
+
+                                        $sHtml .= getLinkPage($nombreDePages, ">>");
+                                    } elseif ($nombreDePages > 1) {
+                                        for ($pageCourante = 1 ; $pageCourante <= $nombreDePages ; $pageCourante++) {
+                                            $sHtml .= getLinkPage($pageCourante)." ";
+                                        }
                                     } else {
-                                        $sHtml .= getLinkPage(2);
-                                        $sHtml .= getLinkPage(3);
-                                        $sHtml .= getLinkPage(4);
-                                        $sHtml .= "...";
-                                    } 
-
-                                    $sHtml .= getLinkPage($nombreDePages);
-
-                                    if ($pageCourante < $nombreDePages) {
-                                        $sHtml .= getLinkPage(($pageCourante+1), ">");
+                                        $sHtml .= getLinkPage(1);
                                     }
 
-                                    $sHtml .= getLinkPage($nombreDePages, ">>");
-                                } elseif ($nombreDePages > 1) {
-                                    for ($pageCourante = 1 ; $pageCourante <= $nombreDePages ; $pageCourante++) {
-                                        $sHtml .= getLinkPage($pageCourante)." ";
-                                    }
-                                } else {
-                                    $sHtml .= getLinkPage(1);
+                                    $sHtml .= "</div>";
+
+                                    $sHtml .= '<a href="back_office.php" class="fb-form-participate-infos fb-admin-link">Administrer le concours</a>';
+
+                                    echo $sHtml;
                                 }
-                                
-                                $sHtml .= "</div>";
-                                
-                                $sHtml .= '<a href="back_office.php" class="fb-form-participate-infos fb-admin-link">Administrer le concours</a>';
-                                
-                                echo $sHtml;
-                            }
-                        ?>
+                            ?>
                         </div>
                     <?php
-                        if (!$session) {
-                            $loginUrl = $helper->getLoginUrl();
-                            
-                            header("Location: ".$loginUrl);
+                            if (!$session) {
+                                $loginUrl = $helper->getLoginUrl();
+
+                                header("Location: ".$loginUrl);
+                            }
                         }
                     ?>
                     </div>
