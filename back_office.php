d73753ae46432458c7317f9c9946fcca92658c67
adriensy <adrien.sy@gmail.com>
2015-07-14 17:56:07 +0200

    Bakc office

--- back_office.php
+++ back_office.php
@@ -159,7 +159,7 @@ $concours = $stmt->fetch(PDO::FETCH_ASSOC);
                                 <input type="hidden" name="formSend" value="1"/>
                                 <div class="fb-form-participate-infos" id="back-office-form">
                                     <div class="form_ligne"><label>Nom du concours : </label><input type="text" name="name" id="name" value="<?php echo $concours['name']; ?>" /></div>
-                                    <div class="form_ligne"><label>Date de début (jj-mm-aaaa) : </label><input type="text" name="dateStart" id="dateStart" value="<?php echo DateTime::createFromFormat('d-m-Y', $concours['date_start']); ?>" /></div>
+                                    <div class="form_ligne"><label>Date de début (jj-mm-aaaa) : </label><input type="text" name="dateStart" id="dateStart" value="<?php echo date('m-d-Y', strtotime($concours['date_start'])); ?>" /></div>
                                     <div class="form_ligne"><label>Date de fin (jj-mm-aaaa) : </label><input type="text" name="dateEnd" id="dateEnd" value="<?php echo $concours['date_end']; ?>" /></div>
                                     <div class="form_ligne"><label for="isStopped" class="label_checkbox">Stopper le concours</a> </label><input type="checkbox" name="isStopped" value="1" <?php echo $concours['is_stopped'] ? 'checked' : ''; ?> id="isStopped"></div>
                                 </div>
