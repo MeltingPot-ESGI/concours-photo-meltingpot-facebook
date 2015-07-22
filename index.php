80e61a3a162e5e9d27a40fc5639d5d4b168ff508
adriensy <adrien.sy@gmail.com>
2015-07-22 15:27:00 +0200

    send to heroku

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ $concours = $stmtConcours->fetch(PDO::FETCH_ASSOC);
             if (!$session) {
         ?>
                 console.log('debut');
-            //FB.getLoginStatus(function() {
+            FB.getLoginStatus(function() {
                 FB.login(function(response) {
                     console.log('test');
                    if (response.authResponse) {
@@ -82,7 +82,7 @@ $concours = $stmtConcours->fetch(PDO::FETCH_ASSOC);
                      console.log('User cancelled login or did not fully authorize.');
                    }
                });
-            //});
+            });
         <?php
             } else {
         ?>
